bcc10f43f11e57fad72ffe7abaac68ba045e0255
Sven Perkmann <sven.perkmann@klarna.com>
2015-08-20 13:59:31 +0200

    updating meteor version

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.0');
+  api.versionsFrom('METEOR@0.9.0');
   api.use('d3js:d3@3.5.6', 'client');
   api.add_files([
     'c3/c3.min.js',
